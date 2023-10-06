de8025e407daee220c1b250f4e8be043a7e5da9e
Jose Maya <jamquelet@hotmail.com>
2023-10-05 20:43:17 -0500

    arreglos de numpy

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Procesamiento de datos con Python
 Repositorio educativo de procesamiento de datos con Python que proporciona recursos y ejemplos prácticos para aprender cómo utilizar Python en análisis de datos, visualización y manipulación de datos. 
+
+Álgebra Lineal con Numpy - Manipulación de datos con Pandas
---
+++ numpy/2arreglosNumPy.py
@@ -0,0 +1,10 @@
+#Conceptos básicos de arreglos de NumPy
+#Atributos de un array
+
+#1.Tamaño de los arreglos: Puedes obtener el tamaño de un arreglo utilizando el atributo size. Este atributo devuelve el número total de elementos en el arreglo.
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print("Tamaño del arreglo:", arr.size)
+
